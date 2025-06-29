3ed3c7c2a4141e52453cfa994f63a2091d9d608a
dobandvd <david.gabriel.doban@gmail.com>
2025-06-29 07:36:38 +0100

    updated port

--- server.py
+++ server.py
@@ -52,4 +52,4 @@ def paypal_webhook():
 # Run Flask app
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(host="0.0.0.0", port=port)
